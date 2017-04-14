e4ac9e201c95bce6b8a619f4240ec5302c5c2bf8
Athou <jeremiepanzer@gmail.com>
2017-04-14 15:57:32 +0200

    initial commit

---
+++ google-music-downloader.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import urllib
+import eyed3
+from gmusicapi import Mobileclient
+
+if len(sys.argv) == 1:
+  print "usage: google-music-downloader <login> <password> <target directory> <album id>"
+  sys.exit()
+
+login = sys.argv[1]
+password = sys.argv[2]
+targetDir = sys.argv[3]
+albumId = sys.argv[4]
+
+eyed3.log.setLevel("ERROR")
+
+api = Mobileclient(debug_logging=False)
+api.login(login, password, Mobileclient.FROM_MAC_ADDRESS)
+
+album = api.get_album_info(albumId)
+dirName = "%s - %s" % (album["artist"], album["name"])
+dirPath = targetDir + "/" + dirName
+
+print("downloading to directory: " + dirPath)
+if not os.path.exists(dirPath):
+    os.makedirs(dirPath)
+	
+for song in album["tracks"]:
+  url = api.get_stream_url(song_id=song["storeId"], quality="hi")
+  fileName = "%s. %s - %s.mp3" % (song["trackNumber"], song["artist"], song["title"])
+  print("downloading: " + fileName)
+  filePath = dirPath + "/" + fileName
+  urllib.urlretrieve(url, filePath)
+  
+  audio = eyed3.load(filePath)
+  if audio.tag is None:
+    audio.tag = eyed3.id3.Tag()
+    audio.tag.file_info = eyed3.id3.FileInfo(filePath)
+  audio.tag.artist = song["artist"]
+  audio.tag.album = album["name"]
+  audio.tag.album_artist = album["artist"]
+  audio.tag.title = song["title"]
+  audio.tag.track_num = song["trackNumber"]
+  audio.tag.save()
+
+print("done.")
